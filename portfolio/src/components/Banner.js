401711c33210b227432da65f27dc6152d1a15f0f
CiroBP <tpa.11.pina@gmail.com>
2024-02-28 23:13:10 -0300

    sobre mi

--- portfolio/src/components/Banner.js
+++ portfolio/src/components/Banner.js
@@ -50,7 +50,7 @@ export const Banner = () =>{
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio</span>
                         <h1>{"Hi i'm fullstack Dev      "}<span className="wrap">--{text}</span></h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        <p>Student in the IT field seeking opportunities to gain experience and put my technical skills and knowledge to the test in a challenging work environment. My passion for quality and efficiency in successful deliveries drives me to contribute significantly to teams and companies.
                         </p>
                         <button onClick={() => console.log('connect')}> Connect with me<ArrowRightCircle size={25}/></button>
                     </Col>
